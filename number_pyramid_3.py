7ddaca378c120134aa92c920e0c71a577fcde908
Nawweh <035218@cirencester.ac.uk>
2025-09-19 12:07:32 +0100

    for loop (number pyramid)

--- number_pyramid_3.py
+++ number_pyramid_3.py
@@ -1,14 +1,20 @@
-space="          "
-count=1
-for i in range (1,11):
-    print(space, end="")
-    space = space[:-1]
+space="                      "
+for i in range (1,11): 
+    print(space,end=" ")
+    space=space[:-2]
 
     for x in range (1,i):
+        print(x, end=" ")  
 
-        print(x, end=" ")
-        
+        if x+1==i:
 
-                
+            while x>0:
+                 x-=1
+                 
+                 if x==0:
+                     break
+                 else:
+                    print(x, end=" ")                 
     print()
-    
+
+
---
+++ number_pyramid_4.py
@@ -0,0 +1,40 @@
+space="                      "
+for i in range (1,11): 
+    print(space,end=" ")
+    space=space[:-2]
+
+    for x in range (1,i):
+        print(x, end=" ")  
+
+        if x+1==i:
+
+            while x>0:
+                 x-=1
+                 
+                 if x==0:
+                     break
+                 else:
+                    print(x, end=" ")                 
+    print()
+    
+space="                      "
+count=11
+for i in range (1,11): 
+    print(space,end=" ")
+    space=space[:-2]
+
+    for z in range (1,i):
+
+        print(i-z, end=" ")
+         
+
+        if x+1==i:
+
+            while x>0:
+                 x-=1
+                 
+                 if x==0:
+                     break
+                 else:
+                    print(x, end=" ")                 
+    print()
