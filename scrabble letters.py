dcf63b00a2976da646c8fdf7faf49f2f71b9d2da
Nawweh <035218@cirencester.ac.uk>
2025-09-26 11:38:22 +0100

    Update scrabble letters.py

--- scrabble letters.py
+++ scrabble letters.py
@@ -58,32 +58,42 @@ def get_random_letters():
     
 
 def game_scrabble():
-    count=0
+    count=1
     valid=False
     p2_score=0
     p1_score=0
 
     print("player 1's turn: ")
-    for i in range(7):
+    for i in range(6):
 
         random_letters=get_random_letters()
-        print(random_letters)
-        print(f"make a word using some or all of the characters in this string: ")
+        for i in random_letters:
+            print(i, end=" ")
+        # print(random_letters)
+        print(f"\n make a word using some or all of the characters in this string: ")
         while valid!=True:
             word=input()
             for letter in word:
                 for x in range (0,7):
                     if letter==random_letters[x]:
                         valid=True
-                            
-        if count%2==0:
-            p2_score+=find_word_value(word)
-            print("now its player 1's turn")
-        else:
-            p1_score+=find_word_value(word)
-            print("now its player 2's turn")
+        if count<6:                    
+            if count%2==0:
+                p2_score+=find_word_value(word)
+                print("now its player 1's turn")
+            else:
+                p1_score+=find_word_value(word)
+                print("now its player 2's turn")
+                
         count+=1
-        return
+        valid=False
+        
+    if p1_score>p2_score:
+        print(f"p1 wins with a score of {p1_score} to {p2_score}")
+    else:
+        print(f"p2 wins with a score of {p2_score} to {p1_score}")
+    
+
     
 
 
@@ -92,24 +102,24 @@ def game_scrabble():
 
 
 
-# def main():
-#     option=0
-#     while option!=4:
-#         option=int(input("Enter 1 to get the value of a scrabble letter \nEnter 2 to get value of a scrabble word \nEnter 3 to play a game \nEnter 4 to quit \n"))
+def main():
+    option=0
+    while option!=4:
+        option=int(input("Enter 1 to get the value of a scrabble letter \nEnter 2 to get value of a scrabble word \nEnter 3 to play a game \nEnter 4 to quit \n"))
 
-#         if option==1:
-#             letter=input("input a letter: ")
-#             letter=str.lower(letter)         
-#             print(find_letter_value(letter))
+        if option==1:
+            letter=input("input a letter: ")
+            letter=str.lower(letter)         
+            print(find_letter_value(letter))
 
-#         elif option==2:
+        elif option==2:
 
-#             word=input("give word: ")
-#             print(find_word_value(word))
+            word=input("give word: ")
+            print(find_word_value(word))
         
-#         elif option==3:
-#             game_scrabble()
-#             option=0
+        elif option==3:
+            game_scrabble()
+            option=0
 
 
-game_scrabble()
+game_scrabble() 
