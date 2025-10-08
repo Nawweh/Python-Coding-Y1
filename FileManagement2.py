9b639cb9c6894d18bdf2c96209026e1284d315be
Nawweh <035218@cirencester.ac.uk>
2025-10-08 10:47:07 +0100

    Update FileManagement2.py

--- FileManagement2.py
+++ FileManagement2.py
@@ -1,3 +1,26 @@
 def fileT2():
-    f=open("f:\\test2.txt","W")
+    valid=False
+
+    f=open("f:\\test2.txt","r")
+
+    username=input("give a username: ")
+    password=input("give a password: ")
+    user_pass=(username,password)
+    print(user_pass)
+
+    for line in f:
+        print(line)
+    
+    for line in f:
+        if line==user_pass:
+            valid=True
+            
+    if valid==True:
+        print("woohoo access!")
+    else:
+        print("no access!")
     f.close()
+
+
+fileT2()
+
