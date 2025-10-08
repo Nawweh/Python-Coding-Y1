3e2c737cab8a1e461055bcfec6b4439bd4760288
Nawweh <035218@cirencester.ac.uk>
2025-10-08 11:02:57 +0100

    Update FileManagement2.py

--- FileManagement2.py
+++ FileManagement2.py
@@ -5,14 +5,17 @@ def fileT2():
 
     username=input("give a username: ")
     password=input("give a password: ")
-    user_pass=(username,password)
+    user_pass=(username+password)
     print(user_pass)
     user_pass_check=[]
     for line in f:
         user_pass_check.append(line)
+
     for item in user_pass_check:
-            if user_pass_check[line]==user_pass:
-            valid="True"
+        print(item)
+        if item==user_pass:
+            valid="true"
+
             
     if valid=="True":
         print("woohoo access!")
