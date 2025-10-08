743081eaaf6a6666abff90df4c10edc686e54abb
Nawweh <035218@cirencester.ac.uk>
2025-10-08 11:21:59 +0100

    Update FileManagement2.py

--- FileManagement2.py
+++ FileManagement2.py
@@ -1,3 +1,5 @@
+def admin_prompt():
+    f=open("f:\\test2.txt","a")
 def fileT2():
     valid="False"
 
@@ -6,23 +8,38 @@ def fileT2():
     username=input("give a username: ")
     password=input("give a password: ")
     user_pass=(username+password)
-    print(user_pass)
     user_pass_check=[]
     for line in f:
-        user_pass_check.append(line)
-
-    for item in user_pass_check:
-        print(item)
-        if item==user_pass:
-            valid="true"
+        
+        if user_pass=="imaadmin":
+            valid="admin"
+            break
+        elif line==user_pass:
+            valid="True"
+    f.close()
 
             
-    if valid=="True":
+    if valid=="admin":
+        print("you an admin")
+        admin_prompt()
+    elif valid=="True":
         print("woohoo access!")
+    
     else:
         print("no access!")
+
+
+admin_prompt()
+
+def admin_prompt():
+    f=open("f:\\test2.txt","a")
+    append="true"
+    while append=="true":
+        user_add=input("add a new username: ")
+        pass_add=input("add a new password: ")
+        user_pass_add=(user_add+pass_add)
+        print("user_pass_add")
     f.close()
 
 
-fileT2()
 
