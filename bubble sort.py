a601fad61c38bf6e420d7bde146bacfeda4e0c95
Nawweh <035218@cirencester.ac.uk>
2025-10-09 10:49:37 +0100

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,14 +1,22 @@
-unsorted=[7,4,1,98,5]
+unsorted=[7,4,1,98,5,6,89,123,345,8]
 sorted=[]
 count=0
 hold=0
-for i in (0,30):
+string_length=len(unsorted)
+count2=5
+for i in unsorted:
     count=0
-    for x in range (0,4):
-        if unsorted[x+1]>unsorted[x]:
+    for x in range (string_length-1):
+        if unsorted[x+1]>unsorted[x]: #checks if the number adjacent to the right in the list is larger
             hold=unsorted[count+1]
-            unsorted[count+1]=unsorted[count]
+            unsorted[count+1]=unsorted[count] #if it is larger the 2 numbers are swapped around (bubble sort)
             unsorted[count]=hold
-            print(count)
+        print(count)
         count+=1
-print(unsorted)
+
+for i in unsorted+1:
+    sorted=unsorted[-i]
+
+
+
+print(sorted)
