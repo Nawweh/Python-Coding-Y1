1b8f607c484a45e439e5fe44435f89cdf03ce7d7
Nawweh <035218@cirencester.ac.uk>
2025-11-05 10:15:13 +0000

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -4,11 +4,9 @@ found=False
 while found==False:
     len_list=(len(list))
     if list[int(len_list//2)]>num:
-          list2=[] 
-          print("hi")      
-          for i in range (int(len_list//2)):
-              list2.append(list[i])
-              print(list2)
-              
-          break
-        
+        list= list[0:len_list//2]
+    elif list[int(len_list//2)]<num:
+        list= list[len_list//2:]
+    else:
+        found=True
+print("the number has been found")
